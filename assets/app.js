423344e30cd64656af27d8bb3f705d0c9927430d
Juras Norkus <jn@drivr.com>
2015-09-01 18:45:13 +0300

    chore(demo): initial demo page commit

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+angular.module('dd.common.demo', ['dd.common', 'plunker', 'ngTouch', 'ngAnimate'], function ($httpProvider) {
+	FastClick.attach(document.body);
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}).run(['$location', function ($location) {
+			//Allows us to navigate to the correct element on initialization
+			if ($location.path() !== '' && $location.path() !== '/') {
+				smoothScroll(document.getElementById($location.path().substring(1)), 500, function (el) {
+					location.replace(el.id);
+				});
+			}
+		}
+	]);
+
+angular.module('dd.common.demo').controller('MainCtrl', ['$scope', function MainCtrl($scope) {
+	
+}]);
