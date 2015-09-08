2f90d9e1c8051fa0524c9cfaedb3fe89b889c259
Juras Norkus <jn@drivr.com>
2015-09-08 16:11:33 +0300

    chore(tests): updated packages, test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,62 @@
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '.',
 
-// base path, that will be used to resolve files and exclude
-basePath = '.';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'misc/test-lib/jquery-1.8.2.min.js',
-  'misc/test-lib/angular.js',
-  'misc/test-lib/angular-mocks.js',
-  'misc/test-lib/helpers.js',
-  'src/**/*.js',
-  'template/**/*.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'misc/test-lib/jquery-1.8.2.min.js',
+      'misc/test-lib/angular.js',
+      'misc/test-lib/angular-mocks.js',
+      'misc/test-lib/angular-growl.js',
+      'src/**/*.js',
+      'template/**/*.js'
+    ],
 
-// list of files to exclude
-exclude = [
-  'src/**/docs/*'
-];
+    // list of files to exclude
+    exclude: [
+      'src/**/docs/*'
+    ],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = [
-  'PhantomJS'
-];
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari
+    // - PhantomJS
+    browsers: [
+      'PhantomJS'
+    ],
 
-// test results reporter to use
-// possible values: dots || progress
-reporters = ['progress'];
+    // test results reporter to use
+    // possible values: dots || progress
+    reporters: ['progress'],
 
-reportSlowerThan = 100;
+    reportSlowerThan: 100,
 
-// web server port
-port = 9018;
+    // web server port
+    port: 9018,
 
-// cli runner port
-runnerPort = 9100;
+    // cli runner port
+    runnerPort: 9100,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
 
+  });
+};
