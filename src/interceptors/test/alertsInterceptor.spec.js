a65115a5ebdabc9d38776bd6f3a882c670f3b7b7
Alvis Davidavicius <ad@drivr.com>
2016-07-22 11:12:52 +0300

    style(test): trailing spaces

--- src/interceptors/test/alertsInterceptor.spec.js
+++ src/interceptors/test/alertsInterceptor.spec.js
@@ -31,7 +31,7 @@
             var reason = { status: 400, data: { message: 'user msg', systemMessage: 'system msg' }, config: { } };
             alertsInterceptor.responseError(reason);
             expect(growler.error).toHaveBeenCalledWith(reason.data.message);
-        });        
+        });
 
         it('shows system message if no message.', function () {
             var reason = { status: 400, data: { systemMessage: 'system msg' }, config: { } };
